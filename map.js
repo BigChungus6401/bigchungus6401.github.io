803615db683bffdc0ca890f4080448ea32a2ec96
Bence Nagy <122285506+BigChungus6401@users.noreply.github.com>
2025-12-05 01:17:32 -0500

    Update map.js

--- map.js
+++ map.js
@@ -1,49 +1,64 @@
-// MapLibre
-const map = new maplibregl.Map({
-	container: "map",
-	style: "https://api.maptiler.com/maps/streets-v2/style.json?key=O25HJX84zRELhSSf1dc4",
-	center: [-86.91406, 40.42725],
-	zoom: 15.75
-});
-
-map.on("load", () => {
-	// Custom HTML + CSS Markers
-	for (var i=0; i<PLACES.length; i++) {
-		let occupancy = Math.random();
-		
-		// Marker Element
-		let m = document.createElement("div");
-		m.className = "custom-marker";
-		if      (occupancy < 0.51) m.style.borderColor = "#2D9B2B";
-		else if (occupancy < 0.66) m.style.borderColor = "#FFD700";
-		else if (occupancy < 0.81) m.style.borderColor = "#FFA500";
-		else if (occupancy < 0.96) m.style.borderColor = "#D40000";
-		
-		// Adding and decorating marker
-		new maplibregl.Marker({element: m}).setLngLat([PLACES[i][2], PLACES[i][1]]).addTo(map)
-			.setPopup(new maplibregl.Popup({offset: 25}).setHTML(
-				`<div style="font-size: 12.5px; line-height: 1.15;">
-					<strong style="font-size: 20px;">${PLACES[i][0]}</strong>
-					<hr>
-					<em style="font-size: 12px; margin-bottom: 20px;">${PLACES[i][4]}</em><br><br>
-					<strong>Occupancy: </strong>${Math.floor(occupancy*PLACES[i][3])}/${PLACES[i][3]}<br>
-					<strong>Sound-Level: </strong>${noiseScale[Math.floor(Math.random()*4)]}<br>
-					<strong>Light-Level: </strong>${lightScale[Math.floor(Math.random()*4)]}
-				</div>`
-			)
-		);
-	}
-	
-	// Geolocation
-	const geoLoc = new maplibregl.GeolocateControl({
-		positionOptions: {
-			enableHighAccuracy: true
-		},
-		trackUserLocation: false,
-		showAccuracyCircle: true
+window.onload = function() {
+	// MapLibre
+	const map = new maplibregl.Map({
+		container: "map",
+		style: "https://api.maptiler.com/maps/streets-v2/style.json?key=O25HJX84zRELhSSf1dc4",
+		//style: "https://api.maptiler.com/maps/basic-v2/style.json?key=O25HJX84zRELhSSf1dc4",
+		//style: "https://api.maptiler.com/maps/dataviz/style.json?key=O25HJX84zRELhSSf1dc4",
+		center: [-86.91406, 40.42725],
+		zoom: 15.75
 	});
 	
-	map.addControl(geoLoc);
-	
-	geoLoc.on("trackuserlocationstart", () => {geoLoc.trigger()});
-});
+	map.on("load", () => {
+		
+		// Custom HTML + CSS Markers
+		for (var i=0; i<PLACES.length; i++) {
+			let occupancy = Math.random();
+			
+			// Marker Element
+			let m = document.createElement("div");
+			m.className = "custom-marker";
+			if      (occupancy < 0.51) m.style.borderColor = "#2D9B2B";
+			else if (occupancy < 0.66) m.style.borderColor = "#FFD700";
+			else if (occupancy < 0.81) m.style.borderColor = "#FFA500";
+			else if (occupancy < 0.96) m.style.borderColor = "#D40000";
+			
+			// Adding and decorating marker
+			new maplibregl.Marker({element: m}).setLngLat([PLACES[i][2], PLACES[i][1]]).addTo(map)
+				.setPopup(new maplibregl.Popup({offset: 25}).setHTML(
+					`<div style="font-size: 12.5px; line-height: 1.15;">
+						<strong style="font-size: 20px;">${PLACES[i][0]}</strong>
+						<hr>
+						<em style="font-size: 12px; margin-bottom: 20px;">${PLACES[i][4]}</em><br><br>
+						<strong>Occupancy: </strong>${Math.floor(occupancy*PLACES[i][3])}/${PLACES[i][3]}<br>
+						<strong>Sound-Level: </strong>${noiseScale[Math.floor(Math.random()*4)]}<br>
+						<strong>Light-Level: </strong>${lightScale[Math.floor(Math.random()*4)]}
+					</div>`
+				)
+			);
+		}
+		
+		// Geolocation
+		const geoLoc = new maplibregl.GeolocateControl({
+			positionOptions: {
+				enableHighAccuracy: true
+			},
+			trackUserLocation: false,
+			showAccuracyCircle: true
+		});
+		
+		const button = geoLoc.onAdd(map);//.getElementsByClassName("maplibregl-ctrl-geolocate");
+		console.log(button);
+		
+		let Location = document.getElementsByClassName("maplibregl-ctrl-top-right")[0];
+		
+		// iOS geolocation fix
+		button.addEventListener('click', () => {
+			geoLoc.trigger();
+		});
+		
+		Location.appendChild(button);
+		
+		//map.addControl(geoLoc);
+	});
+}
