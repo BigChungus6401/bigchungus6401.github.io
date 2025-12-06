2c60b29b5ee9b5c0d229fb1b17972822daa72eb2
Bence Nagy <122285506+BigChungus6401@users.noreply.github.com>
2025-12-06 18:59:39 -0500

    Update map.js

--- map.js
+++ map.js
@@ -1,5 +1,5 @@
-var leftMenuToggle = true;
-var preferencesMenuToggle = true;
+var leftMenuToggle = false;
+var preferencesMenuToggle = false;
 var S1, I1, S2, I2, S3, I3;
 
 window.onload = function() {
@@ -39,14 +39,18 @@ window.onload = function() {
 		// Custom HTML + CSS Markers
 		for (var i=0; i<PLACES.length; i++) {
 			let occupancy = Math.random();
+			let noise = Math.floor(Math.random()*4);
+			let sound = Math.floor(Math.random()*3);
 			
 			// Marker Element
 			let m = document.createElement("div");
 			m.className = "custom-marker";
-			if      (occupancy < 0.51) m.style.borderColor = "#2D9B2B";
-			else if (occupancy < 0.66) m.style.borderColor = "#FFD700";
-			else if (occupancy < 0.81) m.style.borderColor = "#FFA500";
-			else if (occupancy < 0.96) m.style.borderColor = "#D40000";
+			m.id = "marker" + i;
+			if      (occupancy < 0.51) m.style.borderColor = "#2D9B2B", placeData.push([0, noise, sound]);
+			else if (occupancy < 0.66) m.style.borderColor = "#FFD700", placeData.push([1, noise, sound]);
+			else if (occupancy < 0.81) m.style.borderColor = "#FFA500", placeData.push([2, noise, sound]);
+			else if (occupancy < 0.96) m.style.borderColor = "#D40000", placeData.push([3, noise, sound]);
+			else placeData.push([4, noise, sound])
 			
 			// Create Popup
 			let Popup = new maplibregl.Popup({offset: 15});
@@ -55,8 +59,8 @@ window.onload = function() {
 				<hr style="border: 0; background-color: #CFB991; height: 2px;">
 				<em style="font-size: 12px; margin-bottom: 20px;">${PLACES[i][4]}</em><br><br>
 				<strong>Occupancy: </strong>${Math.floor(occupancy*PLACES[i][3])}/${PLACES[i][3]}<br>
-				<strong>Sound-Level: </strong>${noiseScale[Math.floor(Math.random()*4)]}<br>
-				<strong>Light-Level: </strong>${lightScale[Math.floor(Math.random()*4)]}
+				<strong>Sound-Level: </strong>${noiseScale[noise]}<br>
+				<strong>Light-Level: </strong>${lightScale[sound]}
 			</div>`);
 			
 			// Adding and decorating marker
@@ -83,13 +87,17 @@ window.onload = function() {
 function toggleLeftMenu() {
 	if (!leftMenuToggle) {
 		document.getElementById("leftMenu").style.width = "250px";
-		document.getElementById("map").style.left = "250px";
-		document.getElementById("map").style.width = "calc(100% - 250px)";
+		if (window.innerWidth > 650) {
+			document.getElementById("map").style.left = "250px";
+			document.getElementById("map").style.width = "calc(100% - 250px)";
+		}
 		leftMenuToggle = true;
 	} else {
 		document.getElementById("leftMenu").style.width = "0";
-		document.getElementById("map").style.left = "0";
-		document.getElementById("map").style.width = "100%";
+		if (window.innerWidth > 650) {
+			document.getElementById("map").style.left = "0";
+			document.getElementById("map").style.width = "100%";
+		}
 		if (preferencesMenuToggle) togglePreferncesMenu();
 		leftMenuToggle = false;
 	}
@@ -110,7 +118,19 @@ function togglePreferncesMenu() {
 }
 
 function applyPrefs() {
-	console.log("Preferences Applied");
+	for (var i=0; i<PLACES.length; i++) {
+		document.getElementById("marker" + i).classList.remove("BAD");
+	}
+	
+	let occupPref = S1.value; // Occupancy:  < selected
+	let noisePref = S2.value; // Loudness:   < selected
+	let lightPref = S3.value; // Brightness: = selection
+	
+	for (var i=0; i<PLACES.length; i++) {
+		if (placeData[i][0] > occupPref || placeData[i][1] > noisePref || placeData[i][2] != lightPref) {
+			document.getElementById("marker" + i).classList.add("BAD");
+		}
+	}
 }
 
 function resetPrefs() {
@@ -118,6 +138,10 @@ function resetPrefs() {
 	I1.innerText = occupancyScale[1];
 	S2.value = 1;
 	I2.innerText = noiseScale[1];
-	S3.value = 2;
-	I3.innerText = lightScale[2];
+	S3.value = 1;
+	I3.innerText = lightScale[1];
+	
+	for (var i=0; i<PLACES.length; i++) {
+		document.getElementById("marker" + i).classList.remove("BAD");
+	}
 }
